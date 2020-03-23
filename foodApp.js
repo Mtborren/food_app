a4d8d8bc090e421e723d89d1ab16e5d465602918
Tyler <mtborren@gmail.com>
2020-03-23 15:41:48 -0400

    finished homework

--- foodApp.js
+++ foodApp.js
@@ -58,40 +58,24 @@ let menu = {
 
 
 
-
-//trying to keep array and trim but keeps splitting up array instead. moving ahead for now.
-// function addOrder(){
-//     let order = document.getElementById("userInput").value;
-//     let customerOrder = order.trim().split(",");
-//     for(i = 0; i < customerOrder.length; i++){
-//         let ticket = customerOrder[i].trim().split(":");
-//         for(j = 0; j < ticket.length; j++){
-//             kitchen.push(ticket[j].trim());
-//             console.log(kitchen[kitchen.length -1]);
-//         }
-//     }
-//     document.getElementById("userInput").value='';
-// }
-
-
-
-
 function addOrder(){
     let order = document.getElementById("userInput").value;
     let customerOrder = order.trim().split(",");
     for(i = 0; i < customerOrder.length; i++){
         let ticket = customerOrder[i].trim().split(":");
         kitchen.push(ticket);
-        for(j = 0; j < ticket.length; j++){
-            let receipt = ticket[j].trim();
-            console.log(receipt);
-            for(items in menu){
-                // let items = ticket[j];
-                // if(menu.hasOwnProperty(items)){
-                //     console.log("It will be right out!")
-                // } else {
-                //     console.log("We do not make that.")
-                // }
+        let receipt = kitchen[kitchen.length - 1];
+        console.log(receipt);
+        for(items in menu){
+            if(menu.hasOwnProperty(items)){
+                if(menu[items] > 0){
+                    alert("added to cart")
+                    menu[items]--;
+                } else {
+                    alert("Sorry, we're out of stock!")
+                }
+            } else {
+                alert("we cant make that");
             }
         }
     }
@@ -100,8 +84,6 @@ function addOrder(){
 
 
 
-
-//function to clear kitchen array and present what the customer has ordered
 function sendToCook(){
     alert(kitchen);
     while(kitchen.length > 0){
@@ -110,10 +92,6 @@ function sendToCook(){
 }
 
 
-
-
-
-//function to show menu object
 function showMenu(){
     alert(JSON.stringify(menu));
 }
